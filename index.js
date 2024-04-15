5a4771d55ac1ff1e46f7e03b5e5dff736fcf6d15
Sameer Anis <159227804+Sameer-504@users.noreply.github.com>
2024-04-15 16:05:04 +0500

    Create Todos List Project

---
+++ index.js
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoQuestion = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input",
+            message: "What would you like to add in your Todo?",
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: "Would you like to add more in your Todo?",
+            default: "true",
+        },
+    ]);
+    todos.push(todoQuestion.firstQuestion);
+    console.log(todos);
+    // the loop is running on the based of this variable condition
+    condition = todoQuestion.secondQuestion;
+}
+// Read, update, Delete, ADD     Homework
